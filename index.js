7d634d92ac761436055687afd582686e7bed54c7
john keru <keruk355@gmail.com>
2024-09-18 10:46:36 +0800

    package.json updated

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-
+require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors({ origin: process.env.CLIENT_URL }))
+
 app.get('/', (req, res) => {
     res.json({ message: 'from server' })
 })
+
 app.listen(5000, () =>
     console.log('Server is listening on: http://localhost:5000'))
