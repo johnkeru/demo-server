4a2e5f384a32b6f33eeea62037b0cea0bd1ee235
john keru <keruk355@gmail.com>
2024-09-18 15:12:46 +0800

    package.json updated

---
+++ configs/dbConnect.js
@@ -0,0 +1,8 @@
+export default async () => {
+    try {
+        await require('mongoose').connect(process.env.MONGOURL)
+        console.log('Yey connected!')
+    } catch (e) {
+        console.log('Failed to connect: ')
+    }
+}
