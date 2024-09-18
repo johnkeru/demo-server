0087e8edf775a307c7e5d2c84d823e59da1bfb95
john keru <keruk355@gmail.com>
2024-09-18 15:24:29 +0800

    package.json updated

--- models/User.js
+++ models/User.js
@@ -7,19 +7,19 @@ const userSchema = mongoose.Schema({
     password: { type: String, required: true },
 })
 
-userSchema.pre('save', async function (next) {
+userSchema.pre('save', async function (next) { // this is middleware or basically handling save before the actual save to the database
     try {
         const user = this
-        if (!user.isModified('password')) return next();
-        const hashedPassword = await bcrypt.hash(user.password, 10)
-        user.password = hashedPassword
-        next()
+        if (!user.isModified('password')) return next(); // only hash the password if new user or user change the password
+        const hashedPassword = await bcrypt.hash(user.password, 10) // hash the pass: ex. from pass to 322fafawe3w3fwe something that we dont understand
+        user.password = hashedPassword // then after set the hashed password to the password in user
+        next() // next is a function that say that everything is good so the next handler or actual saving to the db 
     } catch (e) {
-        next(e)
+        next(e) // pass the error to next handler
     }
 })
 
-userSchema.methods.comparePassword = function (password) {
+userSchema.methods.comparePassword = function (password) { // use in login to compare user input password to the hashed password
     try {
         const user = this
         return bcrypt.compareSync(password, user.password)
